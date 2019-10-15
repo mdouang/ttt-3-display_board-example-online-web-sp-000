a47c17f279dba11924d53bff571cc9935d5089fd
Maikaru <maikaru@maikaru.com>
2019-10-15 17:09:14 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,10 @@
- display board
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
+def display_board
+ def say_display_board_ten_times
+  phrase1 = "   |   |   "
+  phrase2 = "-----------"
+  puts phrase1
+  puts phrase2
+  puts phrase1
+  puts phrase2
+  puts phrase1
+ end
