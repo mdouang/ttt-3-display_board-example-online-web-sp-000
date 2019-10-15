0abf66352d7204437510f16b580d8034b0ffa3b7
Maikaru <maikaru@maikaru.com>
2019-10-15 17:05:19 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,5 @@
-// ♥ irb
-001:0 > def display_board
-002:1 >     puts "A Tic Tac Toe Board"
-003:1 >   end
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
